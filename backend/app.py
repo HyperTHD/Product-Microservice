c6ef9b29226cd0345324e08a78f8f6473a19dfe6
Abdulbasid Guled <guled.basid@gmail.com>
2021-10-05 15:31:47 -0400

    Added POST, DELETE, and updated GET Routes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify, request, Response, redirect, url_for
+import json
+from bson.objectid import ObjectId
+from flask import Flask, Response, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -21,13 +23,64 @@ def ping_server():
     return "This route should work if MongoDB connects"
 
 
-@app.route('/products')
+@app.route('/products', methods=['GET'])
 def get_products():
     db = get_db()
-    _products = db.Product.find()
-    products = [{"id": product["id"], "name": product["name"]}
-                for product in _products]
-    return jsonify({"products": products})
+    _products = list(db.Product.find())
+    for product in _products:
+        product["_id"] = str(product["_id"])
+    return Response(
+        response=json.dumps(
+            _products
+        ),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/products/<id>', methods=['GET'])
+def get_products(id):
+    db = get_db()
+    product = db.Product.find_one({"_id": ObjectId(id)})
+    return Response(
+        response=json.dumps(
+            product
+        ),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/products', methods=['POST'])
+def create_product():
+    db = get_db()
+    product = {
+        'name': request.form['name'],
+        'quantity': request.form['quantity'],
+        'price': request.form['price'],
+    }
+    response = db.Product.insert_one(product)
+    print(response.inserted_id)
+    return Response(
+        response=json.dumps(
+            {"Message": "Product created successfully",
+             "id": f"{response.inserted_id}"}
+        ),
+        status=201,
+        mimetype="application/json"
+    )
+
+
+@app.route('/products/<id>', methods=['DELETE'])
+def delete_product(id):
+    db = get_db()
+    response = db.Product.delete_one({"_id": ObjectId(id)})
+    return Response(
+        response=json.dumps(
+            {"Message": "Product deleted successfully", "id": f"{id}"}),
+        status=204,
+        mimetype="application/json"
+    )
 
 
 if __name__ == '__main__':
